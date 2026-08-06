0a66b7dd30c4fd0e2046c9be9c25ec3c11a08880
franciscolaria-commits <francisco.laria.1234@gmail.com>
2026-08-06 10:36:49 -0300

    chore: remove unused scripts, backups and temporary files

--- api/migrate_overrides.py
+++
@@ -1,6 +0,0 @@
-from app.database import engine, Base
-from app.models import EjercicioMediaCoach
-
-print("Creando tabla ejercicios_media_coaches...")
-Base.metadata.create_all(bind=engine, tables=[EjercicioMediaCoach.__table__])
-print("¡Tabla creada!")
