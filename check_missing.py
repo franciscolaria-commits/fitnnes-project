ae900d26158b69d8338ab0c3a612e3f0928b25ab
franciscolaria-commits <francisco.laria.1234@gmail.com>
2026-08-28 22:01:38 -0300

    chore: remove mojibake helper scripts

--- check_missing.py
+++
@@ -1,7 +0,0 @@
-﻿with open("pwa/src/views/StudentEvaluations.jsx", "r", encoding="utf-8") as f:
-    content = f.read()
-
-import re
-matches = re.findall(r'.{0,10}.{0,10}', content)
-for m in set(matches):
-    print("Match:", repr(m))
--- check_verif.py
+++
@@ -1,5 +0,0 @@
-﻿with open("pwa/src/views/StudentDashboard.jsx", "r", encoding="utf-8") as f:
-    content = f.read()
-import re
-print("Matches:", re.findall(r'.{0,10}Autom.{0,10}', content))
-print("Matches2:", re.findall(r'.{0,10}T.cnica.{0,10}', content))
